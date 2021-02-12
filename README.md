5243f86000614ceaf6d58332cae39cbf1bcc24a1
Ben Welman <bwelman@gmail.com>
2021-02-12 11:40:04 +0100

    Toegevoegd: Voorwoord, structuur en opmaak.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # exelanalyse
-Data analyse met Excel
+
+Dit is een online studieboek voor het uitvoeren van data analyse met Microsoft Excel, bedoeld voor studenten in het hoger onderwijs.
+
+Het is geen cursus Excel en ook geen cursus statistiek, maar je leert hoe je met Excel datasets kunt verkennen, onderzoeken en samenvatten.
+
+Er wordt gewerkt met Excel 365 NL, maar voor het grootste deel van de inhoud kun je ook met Excel 2010, 2013, 2016 en 2019 werken.
---
+++ _common.R
@@ -0,0 +1,35 @@
+set.seed(25)
+options(digits = 3)
+
+# packages ---------------------------------------------------------------------
+
+suppressMessages(library(knitr))
+suppressMessages(library(tidyverse))
+suppressMessages(library(kableExtra))
+suppressMessages(library(patchwork))
+suppressMessages(library(scales))
+
+
+# knitr chunk options ----------------------------------------------------------
+
+knitr::opts_chunk$set(
+  comment = "#>",
+  collapse = TRUE,
+  message = FALSE,
+  warning = FALSE,
+  #cache = TRUE,
+  echo = FALSE, # hide code unless otherwise noted in chunk options
+  out.width = "70%",
+  fig.align = 'center',
+  fig.width = 6,
+  fig.asp = 0.618,  # 1 / phi
+  fig.show = "hold"
+)
+
+# knit options -----------------------------------------------------------------
+
+options(knitr.kable.NA = "")
+
+# kableExtra options -----------------------------------------------------------
+
+options(kableExtra.html.bsTable = TRUE)
